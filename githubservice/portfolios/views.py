2d03882d35f89501575c824a9bd979d3afdb3a2f
heejung-choi <choi951008@naver.com>
2020-06-29 18:24:37 +0900

    githut api연동 사용자 정보 받아오기

--- githubservice/portfolios/views.py
+++ githubservice/portfolios/views.py
@@ -3,6 +3,7 @@ import requests
 from pprint import pprint
 from datetime import datetime
 from django.http import JsonResponse 
+from requests.auth import HTTPBasicAuth
 import json
 
 #for a in d[0]['ssh_url']:
@@ -11,28 +12,24 @@ import json
 def index(request):
     # username = request.GET['username']
     # url = 'https://api.github.com/users/%s' % username
-    url = 'https://api.github.com/users/heejung-choi'
-    response = requests.get(url).json()
-    repo_url = 'https://api.github.com/users/heejung-choi/repos'       
-    repo_load = json.loads(requests.get(repo_url).text)    
-
-    #pprint(d[0])
-    #pprint(d[0]['name'])
-    #pprint(d[0]['html_url'])
-    #pprint(len(d))
+    url = 'https://api.github.com/users/SeongjinOliver'
+    response = requests.get(url, auth=HTTPBasicAuth('9cab848980beedfbdecb', 'a365a80d1e78e483f242d4a440b943509e4e4b85')).json()
+    print(response)
+    repo_url = 'https://api.github.com/users/SeongjinOliver/repos'       
+    repo_load = json.loads(requests.get(repo_url, auth=HTTPBasicAuth('9cab848980beedfbdecb', 'a365a80d1e78e483f242d4a440b943509e4e4b85')).text)    
 
     repo_name = []
     repo_url = []
     for r in range(len(repo_load)):
         #pprint(repo_load[r]['name'])
         #pprint(repo_load[r]['html_url'])
-        repo_name.append(repo_load[r]['name']+": "+repo_load[r]['html_url'])
+        #repo_name.append(repo_load[r]['name']+": "+repo_load[r]['html_url'])
+        repo_name.append(repo_load[r]['name'])
         repo_url.append(repo_load[r]['html_url'])   
     
     pprint("---------------------")
-    pprint(repo_name)  
+    #pprint(repo_name)
 
-   # print(repo_response[0].values('name'))
     return render(request,'portfolios/index.html',{
         'name': response['login'],
         'profile_img_url': response['avatar_url'],
