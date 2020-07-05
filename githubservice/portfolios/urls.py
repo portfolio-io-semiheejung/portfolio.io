48e43c5908435b58f9cdb2c2475f9610ad3103e4
heejung-choi <choi951008@naver.com>
2020-07-05 16:33:47 +0900

    template skills 분리

--- githubservice/portfolios/urls.py
+++ githubservice/portfolios/urls.py
@@ -4,6 +4,7 @@ from . import views
 app_name = 'portfolios'
 urlpatterns = [
     path('', views.index, name='index'),
+    path('skill/', views.skill, name ='skill'),
     path('about/', views.about, name='about'),
     path('insert/', views.insert, name='insert'),
     path('<int:Usercontent_pk>/', views.detail, name='detail'),
